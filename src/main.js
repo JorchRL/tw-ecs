8039750babd2948e79c1cbc0e2b82c05329bce73
Jorge Romero Lagunas <jorch.rl@gmail.com>
2024-05-13 21:18:01 -0600

    Inital setup

---
+++ src/main.js
@@ -0,0 +1,14 @@
+export class Entity {}
+
+export class Compnent {}
+
+export class System {}
+
+export class ECS {
+  constructor() {
+    this.name = "ECS World";
+  }
+  testDescribe() {
+    return this.name;
+  }
+}
